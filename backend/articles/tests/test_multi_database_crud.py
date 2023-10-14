91a69414fd14f56588c064a72f6d6321bfda6b5b
GytBalt5 <gytisbaltasevicius@gmail.com>
2023-10-14 20:06:57 +0300

    Refactored backend tests code.

--- backend/articles/tests/test_multi_database_crud.py
+++ backend/articles/tests/test_multi_database_crud.py
@@ -1,22 +1,23 @@
 from django.test import TestCase
 
-from core.utils.router import ArticleRouter
 from users.utils import create_super_user
-from articles.models import Article
 from news_feed.utils import create_category
 from articles.utils import create_article
+from articles.models import Article
+from core.utils.router import ArticleRouter
 
 
-class DBShardingCRUDTestCase(TestCase):
+class ArticlesDBShardingBaseTestCase(TestCase):
+
     databases = "__all__"
 
     def setUp(self):
-        """Set up test environment with articles, categories, and users."""
+        """Set up test environment for each test method."""
         self.article_router = ArticleRouter()
         self.article_count = 22
         self.articles = []
-        article_category_id = create_category("GENERAL-ARTICLE", "General articles").id
-        user_id = create_super_user().id
+        self.article_category_id = create_category("GENERAL-ARTICLE", "General articles").id
+        self.user_id = create_super_user().id
         for idx in range(1, self.article_count + 1):
             self.articles.append(
                 create_article(
@@ -24,12 +25,19 @@ class DBShardingCRUDTestCase(TestCase):
                     content=f"ARTICLE {idx + 1}",
                     is_featured=True,
                     is_published=True,
-                    category_id=article_category_id,
-                    user_id=user_id,
+                    category_id=self.article_category_id,
+                    user_id=self.user_id,
                 )
             )
 
-    def test_articles_amount(self):
+    def tearDown(self):
+        """Clean up after each test method."""
+        Article.objects.all().delete()
+
+
+class DBShardingCRUDTestCase(ArticlesDBShardingBaseTestCase):
+
+    def test_should_created_articles_amount_be_as_expected_amount(self):
         """Ensure the correct number of articles are created and distributed across databases."""
         self.assertEqual(self.article_count, len(self.articles))
         total_articles = sum(
@@ -38,11 +46,10 @@ class DBShardingCRUDTestCase(TestCase):
         )
         self.assertEqual(self.article_count, total_articles)
 
-    def test_data_creation(self):
+    def test_should_each_article_be_saved_to_expected_shard(self):
         """Validate that each article is saved in the expected shard."""
         article_category_id = create_category("GENERAL-ARTICLE", "General articles").id
         user_id = create_super_user().id
-        temp_articles = []
         for idx in range(1, self.article_count):
             p = create_article(
                 title=f"ARTICLE {idx + 1}",
@@ -54,13 +61,8 @@ class DBShardingCRUDTestCase(TestCase):
             )
             expected_db = self.article_router.db_for_write(Article, hints={"uid": p.uid})
             self.assertEqual(expected_db, p._state.db)
-            temp_articles.append(p)
 
-        # Clean up temporary articles after test.
-        for article in temp_articles:
-            article.delete()
-
-    def test_data_retrieval(self):
+    def test_should_each_article_be_retrieved_from_expected_shard(self):
         """Test if data can be correctly retrieved from the expected database."""
         for article in self.articles:
             expected_db = self.article_router.db_for_read(Article, hints={"uid": article.uid})
@@ -70,7 +72,7 @@ class DBShardingCRUDTestCase(TestCase):
             self.assertIsNotNone(retrieved_article)
             self.assertEqual(article.uid, retrieved_article.uid)
 
-    def test_data_update(self):
+    def test_should_each_article_be_updated_in_expected_shard(self):
         """Test if data can be correctly updated in the expected database."""
         for article in self.articles:
             expected_title = f"Updated Title {article.uid}"
@@ -80,7 +82,7 @@ class DBShardingCRUDTestCase(TestCase):
             updated_article = Article.objects.using(expected_db).get(uid=article.uid)
             self.assertEqual(expected_title, updated_article.title)
 
-    def test_data_deletion(self):
+    def test_should_each_article_be_deleted_from_expected_shard(self):
         """Test if data can be correctly deleted from the expected database."""
         for article in self.articles:
             article_uid = article.uid
@@ -96,42 +98,31 @@ class DBShardingCRUDTestCase(TestCase):
         self.assertEqual(0, total_articles)
 
 
-class DBShardingCRUDAutoSelectingDBTestCase(TestCase):
-    databases = "__all__"
+class DBShardingCRUDAutoSelectingDBTestCase(ArticlesDBShardingBaseTestCase):
 
-    def setUp(self):
-        """Set up test environment with articles, categories, and users."""
-        self.article_router = ArticleRouter()
-        self.article_count = 22
-        self.articles = []
-        article_category_id = create_category("GENERAL-ARTICLE", "General articles").id
-        user_id = create_super_user().id
-        for idx in range(1, self.article_count + 1):
-            self.articles.append(
-                create_article(
-                    title=f"ARTICLE {idx + 1}",
-                    content=f"ARTICLE {idx + 1}",
-                    is_featured=True,
-                    is_published=True,
-                    category_id=article_category_id,
-                    user_id=user_id,
-                )
-            )
-
-    def test_articles_amount(self):
-        """Ensure the correct number of articles are created."""
+    def test_should_created_articles_amount_be_as_expected_amount(self):
+        """
+        Note. DB should be auto selected.
+        Ensure the correct number of articles are created.
+        """
         self.assertEqual(self.article_count, len(self.articles))
         self.assertEqual(self.article_count, Article.objects.count())
 
-    def test_data_retrieval_auto_selecting_db(self):
-        """Test if data can be correctly retrieved without explicitly selecting the database."""
+    def test_should_each_article_be_retrieved_from_expected_shard(self):
+        """
+        Note. DB should be auto selected.
+        Test if data can be correctly retrieved without explicitly selecting the database.
+        """
         for article in self.articles:
             retrieved_article = Article.objects.filter(uid=article.uid).first()
             self.assertIsNotNone(retrieved_article)
             self.assertEqual(article.uid, retrieved_article.uid)
 
-    def test_data_update_auto_selecting_db(self):
-        """Test if data can be correctly updated without explicitly selecting the database."""
+    def test_should_each_article_be_updated_in_expected_shard(self):
+        """
+        Note. DB should be auto selected.
+        Test if data can be correctly updated without explicitly selecting the database.
+        """
         for article in self.articles:
             expected_title = f"Updated Title {article.uid}"
             article.title = expected_title
@@ -140,8 +131,11 @@ class DBShardingCRUDAutoSelectingDBTestCase(TestCase):
             self.assertIsNotNone(updated_article)
             self.assertEqual(expected_title, updated_article.title)
 
-    def test_data_deletion_auto_selecting_db(self):
-        """Test if data can be correctly deleted without explicitly selecting the database."""
+    def test_should_each_article_be_deleted_from_expected_shard(self):
+        """
+        Note. DB should be auto selected.
+        Test if data can be correctly deleted without explicitly selecting the database.
+        """
         for article in self.articles:
             article_uid = article.uid
             article.delete()
--- backend/core/tests/test_multi_database_router.py
+++ backend/core/tests/test_multi_database_router.py
@@ -25,11 +25,11 @@ class DBRouterShardingTestCase(TestCase):
         self.expected_db_alias = [ARTICLES_A_DB_ALIAS, ARTICLES_B_DB_ALIAS, ARTICLES_C_DB_ALIAS]
         self.articles_amount = 22
 
-    def test_db_alias_list(self):
+    def test_should_be_equal_router_db_list_as_expected_list(self):
         """Ensure that the router's database aliases correspond to the expected list."""
-        self.assertEqual(self.article_router.db_alias, self.expected_db_alias)
+        self.assertEqual(self.expected_db_alias, self.article_router.db_alias)
 
-    def test_db_for_write(self):
+    def test_should_router_select_correct_db_for_write(self):
         """Check if the router correctly selects the database for write operations based on article UID."""
         for fake_uid in range(1, self.articles_amount + 1):
             expected_db = determine_expected_database_with_modulo_sharding(
@@ -38,7 +38,7 @@ class DBRouterShardingTestCase(TestCase):
             selected_db = self.article_router.db_for_write(Article, hints={"uid": fake_uid})
             self.assertEqual(expected_db, selected_db)
 
-    def test_db_for_read(self):
+    def test_should_router_select_correct_db_for_read(self):
         """Check if the router correctly selects the database for read operations based on article UID."""
         for fake_uid in range(1, self.articles_amount + 1):
             expected_db = determine_expected_database_with_modulo_sharding(
@@ -47,35 +47,27 @@ class DBRouterShardingTestCase(TestCase):
             selected_db = self.article_router.db_for_read(Article, hints={"uid": fake_uid})
             self.assertEqual(expected_db, selected_db)
 
-    def test_allow_migrate(self):
+    def test_should_router_have_correct_permissions_for_allow_migrate(self):
         """Evaluate the router's migration permissions across various app labels and databases."""
         # Check permissions for expected app labels.
         for db in self.expected_db_alias:
             self.assertTrue(self.article_router.allow_migrate(db=db, app_label="articles"))
-            self.assertTrue(
-                self.article_router.allow_migrate(db=db, app_label="contenttypes")
-            )
+            self.assertTrue(self.article_router.allow_migrate(db=db, app_label="contenttypes"))
             self.assertTrue(self.article_router.allow_migrate(db=db, app_label="admin"))
             self.assertIsNone(self.article_router.allow_migrate(db=db, app_label="users"))
             self.assertIsNone(self.article_router.allow_migrate(db=db, app_label="news_feed"))
-            self.assertIsNone(
-                self.article_router.allow_migrate(db=db, app_label="some_app")
-            )
+            self.assertIsNone(self.article_router.allow_migrate(db=db, app_label="some_app"))
 
         # Check permissions for databases not related to article operations.
         for db in [AUTH_DB_ALIAS, NEWS_FEED_DB_ALIAS, "some_db"]:
             self.assertFalse(self.article_router.allow_migrate(db=db, app_label="articles"))
-            self.assertFalse(
-                self.article_router.allow_migrate(db=db, app_label="contenttypes")
-            )
+            self.assertFalse(self.article_router.allow_migrate(db=db, app_label="contenttypes"))
             self.assertFalse(self.article_router.allow_migrate(db=db, app_label="admin"))
             self.assertIsNone(self.article_router.allow_migrate(db=db, app_label="users"))
             self.assertIsNone(self.article_router.allow_migrate(db=db, app_label="news_feed"))
-            self.assertIsNone(
-                self.article_router.allow_migrate(db=db, app_label="some_app")
-            )
+            self.assertIsNone(self.article_router.allow_migrate(db=db, app_label="some_app"))
 
-    def test_select_db_no_hints(self):
+    def test_should_router_select_correct_db_without_hints(self):
         """Assess the router's default database selection in the absence of hints."""
         # Evaluate default selection for write operations.
         selected_db = self.article_router.db_for_write(Article)
