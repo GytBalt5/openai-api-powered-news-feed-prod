14c6597ae9313771b15f4a4a6e1a95f9c636c743
GytBalt5 <gytisbaltasevicius@gmail.com>
2023-10-21 23:17:00 +0300

    Created code in the notebook for analysis, also implementing tokenizer.

--- django_backend/openaiapp/tokenizers.py
+++ django_backend/openaiapp/tokenizers.py
@@ -1,2 +1,7 @@
-def tokenize_text(text: str) -> list:
-    pass
+import tiktoken
+
+
+def tokenize_text(text: str) -> list[list]:
+    # Load the cl100k_base tokenizer which is designed to work with the ada-002 model.
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    return tokenizer.encode(text)
