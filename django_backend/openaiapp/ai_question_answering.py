8978d9a98f5963231c48c4b194651fee9e901547
GytBalt5 <gytisbaltasevicius@gmail.com>
2023-10-25 18:31:41 +0300

    Refactored ai qustion answering tests and implementation.

--- django_backend/openaiapp/ai_question_answering.py
+++ django_backend/openaiapp/ai_question_answering.py
@@ -1,82 +1,123 @@
+from abc import ABC, abstractmethod
+
 import openai
 from openai.embeddings_utils import distances_from_embeddings
+from pandas import DataFrame
 
-from openaiapp.embeddings import create_embedding
 from openaiapp.text_preparation import generate_each_text_of_df_tokens_amount
+from openaiapp.embeddings import create_embedding
 
 
-CONTEXT_MAX_LEN = 1800
-
-
-# NOTE. This functions copy pasted from OpenAI's documentation.
-# TODO. Need refactoring.
-def create_context(question, df, max_len=CONTEXT_MAX_LEN):
+class AbstractAIQuestionAnswering(ABC):
     """
-    Create a context for a question by finding the most similar context from the data frame.
+    An abstract class for answering questions based on the context.
     """
 
-    df = generate_each_text_of_df_tokens_amount(df=df)
+    @abstractmethod
+    def create_context(self, question: str, max_len: int) -> str:
+        """
+        Create a context for a question by finding the most similar context from the data frame.
 
-    # Get the embeddings for the question.
-    q_embeddings = create_embedding(input=question)
+        :param question: The question to create a context for.
+        :param max_len: The maximum length of the context.
+        :return: The context for the question.
+        """
+        pass
 
-    # Get the distances from the embeddings.
-    df["distances"] = distances_from_embeddings(
-        q_embeddings, df["embeddings"].values, distance_metric="cosine"
-    )
+    @abstractmethod
+    def answer_question(self, question: str, max_len: int) -> str:
+        """
+        Answer a question based on the most similar context from the data frame texts.
 
-    returns = []
-    cur_len = 0
-
-    # Sort by distance and add the text to the context until the context is too long.
-    for _, row in df.sort_values("distances", ascending=True).iterrows():
-        # Add the length of the text to the current length.
-        cur_len += row["n_tokens"] + 4
-
-        # If the context is too long, break.
-        if cur_len > max_len:
-            break
-
-        # Else add it to the text that is being returned.
-        returns.append(row["text"])
-
-    # Return the context.
-    return "\n\n###\n\n".join(returns)
+        :param question: The question to answer.
+        :param max_len: The maximum length of the context.
+        :return: The answer to the question.
+        """
+        pass
 
 
-# NOTE. This functions copy pasted from OpenAI's documentation.
-# TODO. Need refactoring.
-def answer_question(
-    df,
-    model="gpt-3.5-turbo-instruct",
-    question="Am I allowed to publish model outputs to Twitter, without a human review?",
-    max_len=1800,
-    debug=False,
-    max_tokens=150,
-    stop_sequence=None,
-):
+class AIQuestionAnsweringBasedOnContext(AbstractAIQuestionAnswering):
     """
-    Answer a question based on the most similar context from the dataframe texts.
+    A class for answering questions based on the context.
     """
-    context = create_context(question, df, max_len=max_len)
-    # If debug, print the raw model response.
-    if debug:
-        print("Context:\n" + context)
-        print("\n\n")
 
-    try:
-        # Create a completions using the question and context.
-        response = openai.Completion.create(
-            prompt=f"Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\"\n\nContext: {context}\n\n---\n\nQuestion: {question}\nAnswer:",
-            temperature=0,
-            max_tokens=max_tokens,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            stop=stop_sequence,
-            model=model,
+    MODEL = "gpt-3.5-turbo-instruct"
+    CONTEXT_MAX_LEN = 2048
+    MAX_TOKENS = 256
+
+    def __init__(self, df: DataFrame, model: str = MODEL, 
+                 max_tokens: int = MAX_TOKENS, stop_sequence: str = None):
+        """
+        Initialize the AIQuestionAnswering object.
+
+        :param df: A pandas DataFrame containing the texts to use for answering questions.
+        :param model: The OpenAI model to use for answering questions.
+        :param max_tokens: The maximum number of tokens to use for generating the answer.
+        :param stop_sequence: The sequence to use for stopping the answer generation.
+        """
+        self.df = df
+        self.model = model
+        self.max_tokens = max_tokens
+        self.stop_sequence = stop_sequence
+
+    def create_context(self, question: str, max_len: int = CONTEXT_MAX_LEN) -> str:
+        """
+        Create a context for a question by finding the most similar context from the data frame.
+
+        :param question: The question to create a context for.
+        :param max_len: The maximum length of the context.
+        :return: The context for the question.
+        """
+        df = generate_each_text_of_df_tokens_amount(df=self.df)
+
+        # Get the embeddings for the question.
+        q_embeddings = create_embedding(input=question)
+
+        # Get the distances from the embeddings.
+        df["distances"] = distances_from_embeddings(
+            q_embeddings, self.df["embeddings"].values, distance_metric="cosine"
         )
-        return response["choices"][0]["text"].strip()
-    except Exception as e:
-        print(e)
-        return ""
+
+        returns = []
+        cur_len = 0
+
+        # Sort by distance and add the text to the context until the context is too long.
+        for _, row in df.sort_values("distances", ascending=True).iterrows():
+            # Add the length of the text to the current length.
+            cur_len += row["n_tokens"]
+
+            # If the context is too long, break.
+            if cur_len > max_len:
+                break
+
+            # Else add it to the text that is being returned.
+            returns.append(row["text"])
+
+        # Return the context.
+        return "\n\n###\n\n".join(returns)
+
+    def answer_question(self, question: str, max_len: int = CONTEXT_MAX_LEN) -> str:
+        """
+        Answer a question based on the most similar context from the data frame texts.
+
+        :param question: The question to answer.
+        :param max_len: The maximum length of the context.
+        :return: The answer to the question.
+        """
+        context = self.create_context(question, max_len=max_len)
+
+        try:
+            # Create a completions using the question and context.
+            response = openai.Completion.create(
+                prompt=f"Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\"\n\nContext: {context}\n\n---\n\nQuestion: {question}\nAnswer:",
+                temperature=0,
+                max_tokens=self.max_tokens,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                stop=self.stop_sequence,
+                model=self.model,
+            )
+            return response["choices"][0]["text"].strip()
+        except Exception as e:
+            raise Exception(f"Error generating answer: {str(e)}")
--- django_backend/openaiapp/tests/test_ai_question_answering.py
+++ django_backend/openaiapp/tests/test_ai_question_answering.py
@@ -1,13 +1,12 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from django.test import TestCase
 
 from pandas import DataFrame
 
 from openaiapp.embeddings import create_embeddings_of_df_text, flatten_embeddings_of_df
-from openaiapp.ai_question_answering import create_context, answer_question
+from openaiapp.ai_question_answering import AbstractAIQuestionAnswering, AIQuestionAnsweringBasedOnContext
 
 
-# TODO. Need more testing (edge cases, etc.), use mocking.
 class AIQuestionAnsweringTestCase(TestCase):
     def setUp(self):
         self.texts = [
@@ -17,33 +16,38 @@ class AIQuestionAnsweringTestCase(TestCase):
         df = DataFrame({"text": self.texts})
         df = create_embeddings_of_df_text(df=df)
         self.df = flatten_embeddings_of_df(df=df)
+        self.ai_qa = AIQuestionAnsweringBasedOnContext(df=self.df)
+
+    def test_should_question_answering_instance_also_be_abstract(self):
+        """
+        Test that the AIQuestionAnswering instance is also an instance of the AbstractAIQuestionAnswering class.
+        """
+        self.assertIsInstance(self.ai_qa, AbstractAIQuestionAnswering)
 
     def test_should_be_created_context_for_question(self):
-        """Should a context for a question be created by finding the most similar context from the data frame."""
-
-        question = "What Csharp programming language pron and cons?"
-        context = create_context(question, self.df, max_len=30)
+        """
+        Test that a context for a question is created by finding the most similar context from the data frame.
+        """
+        question = "What Csharp programming language pron and con?"
+        context = self.ai_qa.create_context(question=question, max_len=30)
 
         self.assertIsInstance(context, str)
         self.assertEqual(context, self.texts[1])
 
     def test_should_answer_question(self):
-        question = "What Matcha tea pron and cons?"
-        answer = answer_question(question=question, df=self.df)
-
-        self.assertIsInstance(answer, str)
-        self.assertEqual(answer, "I don't know")
-
-    def test_should_answer_question_mocking_example(self):
+        """
+        Test if can answer the question.
+        """
         with patch(
-            "openaiapp.ai_question_answering.create_context"
+            f"openaiapp.ai_question_answering.{type(self.ai_qa).__name__}.create_context"
         ) as mock_create_context, patch(
             "openai.Completion.create"
         ) as mock_completion_create:
+            # Mock the create_context and Completion.create methods.
             mock_create_context.return_value = "context"
-            mock_completion_create.return_value = {"choices": [{"text": "answer"}]}
-            question = "What Matcha tea pron and cons?"
-            answer = answer_question(question=question, df=self.df)
+            mock_completion_create.return_value = {"choices": [{"text": "I don't know"}]}
+            question = "What Matcha tea pron and con?"
+            answer = self.ai_qa.answer_question(question=question)
 
             self.assertIsInstance(answer, str)
-            self.assertEqual(answer, "answer")
+            self.assertEqual(answer, "I don't know")
