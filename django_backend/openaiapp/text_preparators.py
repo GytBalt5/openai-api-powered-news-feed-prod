4587efca5f012a3f9f3f5823584f95d78b7ca4a4
GytBalt5 <gytisbaltasevicius@gmail.com>
2023-11-11 15:32:07 +0200

    Refactored code.

--- django_backend/openaiapp/text_preparators.py
+++ django_backend/openaiapp/text_preparators.py
@@ -7,19 +7,25 @@ from pandas import DataFrame
 from openaiapp.tokenizers import tokenizer
 
 
-class AbstractTextPreparator(ABC):
-    @abstractmethod
-    def split_text_into_chunks(text: str, max_tokens: int) -> List[str]:
-        pass
+class AbstractTextPreparatory(ABC):
+    """
+    Abstract base class for text preparation.
+    """
 
     @abstractmethod
-    def check_max_tokens_amount(max_tokens: int, min_amount: int, max_amount: int):
+    def split_text_into_chunks(self, text: str, max_tokens: int) -> List[str]:
+        """
+        Abstract method to split a text into chunks with a maximum token limit.
+        """
         pass
 
 
-class SimpleTextPreparator(AbstractTextPreparator):
-    @staticmethod
-    def split_text_into_chunks(text: str, max_tokens: int) -> List[str]:
+class TextPreparatory(AbstractTextPreparatory):
+    """
+    Concrete class for text preparation.
+    """
+
+    def split_text_into_chunks(self, text: str, max_tokens: int) -> List[str]:
         """
         Split a text into chunks of at most `max_tokens` tokens.
         """
@@ -27,13 +33,9 @@ class SimpleTextPreparator(AbstractTextPreparator):
         sentences = re.split(r"(?<=[.!?])\s+", text)
 
         # Get the token count for each sentence.
-        sentence_token_counts = [
-            (sentence, len(tokenizer.tokenize_text(sentence))) for sentence in sentences
-        ]
+        sentence_token_counts = [(sentence, len(tokenizer.tokenize_text(sentence))) for sentence in sentences]
 
-        chunks = []
-        current_chunk = ""
-        current_chunk_size = 0
+        chunks, current_chunk, current_chunk_size = [], "", 0
 
         for idx, (sentence, size) in enumerate(sentence_token_counts):
             # Check that the sentence token count is within the maximum limit.
@@ -42,71 +44,40 @@ class SimpleTextPreparator(AbstractTextPreparator):
             ), f"The sentence tokens amount is greater than the maximum tokens. Tokens amount {size} > max tokens {max_tokens}."
 
             # Get the token count for the next sentence.
-            _, next_size = (
-                sentence_token_counts[idx + 1]
-                if idx < len(sentence_token_counts) - 1
-                else ("", -1)
-            )
-
-            current_chunk += f" {sentence}" if current_chunk else sentence
+            next_size = sentence_token_counts[idx + 1][1] if idx < len(sentence_token_counts) - 1 else -1
+            current_chunk += (" " + sentence) if current_chunk else sentence
             current_chunk_size += size
 
-            if next_size == -1:  # the last sentence
+            if next_size == -1 or current_chunk_size + next_size > max_tokens:
                 chunks.append(current_chunk)
-            elif (
-                current_chunk_size + next_size > max_tokens
-                and current_chunk_size <= max_tokens
-            ):
-                chunks.append(current_chunk)
-                current_chunk = ""
-                current_chunk_size = 0
+                current_chunk, current_chunk_size = "", 0
 
         return chunks
 
-    @staticmethod
-    def check_max_tokens_amount(max_tokens: int, min_amount: int, max_amount: int):
-        """
-        Check that the maximum token count is within the allowed range.
-        """
-        if max_tokens < min_amount:
-            raise ValueError(
-                f"Tokens amount must be greater or equal to {min_amount}. Passed {max_tokens} max_tokens."
-            )
-        elif max_tokens > max_amount:
-            raise ValueError(
-                f"Tokens amount must be less or equal to {max_amount}. Passed {max_tokens} max_tokens."
-            )
 
+class DataFrameTextPreparatory(TextPreparatory):
+    """
+    Text preparation for DataFrame.
+    """
 
-class DataFrameTextPreparator(SimpleTextPreparator):
-    MIN_TOKENS = 8
-    MAX_TOKENS = 512
-
-    def __init__(self, df: DataFrame):
+    def __init__(self, df: DataFrame, min_tokens: int, max_tokens: int):
+        super().__init__()
         self.df = df
+        self.min_tokens = min_tokens
+        self.max_tokens = max_tokens
 
-    def shorten_texts(self, max_tokens: int = MAX_TOKENS) -> DataFrame:
+    def shorten_texts(self, max_tokens: int = None) -> DataFrame:
         """
-        Shorten the texts based on the maximum token limit.
+        Shorten the texts in the DataFrame based on the maximum token limit.
         """
-        DataFrameTextPreparator.check_max_tokens_amount(
-            max_tokens, self.MIN_TOKENS, self.MAX_TOKENS
-        )
+        max_tokens = max_tokens or self.max_tokens
+        self._check_max_tokens_amount(max_tokens)
+
         shortened_texts = []
-
-        # Loop through the DataFrame.
-        for _, row in self.df.iterrows():
-            text = row["text"]
-
-            # Skip rows where the text is None.
-            if text is None:
-                continue
-
-            # Split the text into chunks if it exceeds the maximum token count.
-            if len(tokenizer.tokenize_text(text)) > max_tokens:
-                shortened_texts.extend(
-                    DataFrameTextPreparator.split_text_into_chunks(text, max_tokens)
-                )
+        for text in self.df["text"].dropna():
+            token_count = len(tokenizer.tokenize_text(text))
+            if token_count > max_tokens:
+                shortened_texts.extend(self.split_text_into_chunks(text, max_tokens))
             else:
                 shortened_texts.append(text)
 
@@ -116,27 +87,14 @@ class DataFrameTextPreparator(SimpleTextPreparator):
         """
         Generate the token count for each text in the DataFrame.
         """
-        self.df["n_tokens"] = self.df.text.apply(
-            lambda x: len(tokenizer.tokenize_text(x))
-        )
+        self.df["n_tokens"] = self.df["text"].apply(lambda x: len(tokenizer.tokenize_text(x)) if x else 0)
         return self.df
 
-
-def get_text_preparator_object(data_object=None) -> AbstractTextPreparator:
-    """
-    Get a text preparator object for the given data object.
-
-    Args:
-        data_object: The data object to prepare text for.
-
-    Returns:
-        An instance of a text preparator object.
-
-    Raises:
-        TypeError: If the data object is not supported.
-    """
-    if data_object is None:
-        return SimpleTextPreparator()
-    elif isinstance(data_object, DataFrame):
-        return DataFrameTextPreparator(data_object)
-    raise TypeError("The data object is not supported.")
+    def _check_max_tokens_amount(self, max_tokens: int):
+        """
+        Check that the maximum token count is within the allowed range.
+        """
+        if max_tokens < self.min_tokens:
+            raise ValueError(f"Max tokens must be ≥ {self.min_tokens}. Given: {max_tokens}.")
+        elif max_tokens > self.max_tokens:
+            raise ValueError(f"Max tokens must be ≤ {self.max_tokens}. Given: {max_tokens}.")
